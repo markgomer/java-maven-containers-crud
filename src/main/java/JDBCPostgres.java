4c6aa55cb7f469ac72bbded7717dbd9f19140942
markgomer <aureliojuniorcmrj@hotmail.com>
2025-07-19 12:40:13 -0300

    Now tiny little code to add new line to database yeah!

--- src/main/java/JDBCPostgres.java
+++ src/main/java/JDBCPostgres.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.Properties;
 
 public class JDBCPostgres {
@@ -11,7 +12,7 @@ public class JDBCPostgres {
       /*
        * usually, you'd want the pg:5432 to be localhost:5432, but I've
        * configured the compose file with the service name "pg", so use that.
-      */
+       */
       String url = "jdbc:postgresql://pg:5432/rocket_db";
 
       Properties props = new Properties();
@@ -21,6 +22,18 @@ public class JDBCPostgres {
 
       Connection conn = DriverManager.getConnection(url, props);
       System.out.println("Conexão realizada com sucesso");
+
+      // let's add a pretty little new line registry to the database
+      String sqlCommand = "INSERT INTO public.tab_cadastro (nome, idade) VALUES(?,?);";
+      String nome = "Mari";
+      int idade = 35;
+
+      PreparedStatement p = conn.prepareStatement(sqlCommand);
+      p.setString(1, nome);
+      p.setInt(2, idade);
+      p.execute();
+
+      System.out.println("Registro inserido com sucesso");
 
     } catch (Exception e) {
       e.printStackTrace();
