2a973ca4976cc14ed2d0eb339f303f5aa1c1072d
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-08-20 15:14:40 -0300

    just some tests

--- src/main/java/SistemaCadastro.java
+++ src/main/java/SistemaCadastro.java
@@ -2,10 +2,13 @@ public class SistemaCadastro {
   public static void main(String[] args) {
     FabricaConexao.conectar();
     CadastroRepository repository = new CadastroRepository();
-    Cadastro cadastro = new Cadastro();
-    cadastro.setNome("Batata");
-    cadastro.setIdade(17);
+    Cadastro cadastro = repository.buscar(4);
+    if (cadastro != null) {
+      System.out.println(cadastro.getId() + " " + cadastro.getNome());
+    } else {
+      System.out.println("Cadastro não localizado");
+    }
 
-    repository.salvar(cadastro);
+    FabricaConexao.close();
   }
 }
