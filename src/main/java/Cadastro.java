c09061216b13f0e61f07d22f9f29585567bea914
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-08-20 14:02:30 -0300

    we need the id in the model

--- src/main/java/Cadastro.java
+++ src/main/java/Cadastro.java
@@ -1,6 +1,15 @@
 public class Cadastro {
+  private Integer id;
   private String nome;
   private int idade;
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
 
   public String getNome() {
     return this.nome;
